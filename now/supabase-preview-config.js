8bba720ed9eb330bc52cfa59edaa76c51e6f23c7
7b964ktxpc-web <7b964ktxpc@privaterelay.appleid.com>
2026-08-25 23:58:09 +0700

    Add public Supabase preview config

---
+++ now/supabase-preview-config.js
@@ -0,0 +1,6 @@
+(() => {
+  window.NowSupabasePreviewConfig = Object.freeze({
+    url: 'https://amyysvcpmbyuxelxixqj.supabase.co',
+    publishableKey: 'sb_publishable_3i7SUzaHZEproIBz4l8K-g_jsOhoiiB',
+  });
+})();
