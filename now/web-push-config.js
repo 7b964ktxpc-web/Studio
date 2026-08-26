e8d36d68bb699c4fa4ff508650c5adbf2433ab7f
7b964ktxpc-web <7b964ktxpc@privaterelay.appleid.com>
2026-08-26 14:56:20 +0700

    Configure Preview Web Push public VAPID key

--- now/web-push-config.js
+++ now/web-push-config.js
@@ -1,6 +1,6 @@
 (() => {
   // Preview-only public configuration. Never put VAPID private keys here.
   window.NowWebPushConfig = Object.freeze({
-    vapidPublicKey: '',
+    vapidPublicKey: 'BPTqGuU4JWkBtynvgVlfhKrjIUtnn5c142ojVM7kt9xiQZsw7sNvDDOkUP1QrtzAx95w-ujtj7vFQEAqOMkIWKg',
   });
 })();
