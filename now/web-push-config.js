9292f86a257edd36a32d1d649f5bce65248247fb
7b964ktxpc-web <7b964ktxpc@privaterelay.appleid.com>
2026-08-26 15:04:47 +0700

    Use valid Preview VAPID public key

--- now/web-push-config.js
+++ now/web-push-config.js
@@ -1,6 +1,6 @@
 (() => {
   // Preview-only public configuration. Never put VAPID private keys here.
   window.NowWebPushConfig = Object.freeze({
-    vapidPublicKey: 'BPTqGuU4JWkBtynvgVlfhKrjIUtnn5c142ojVM7kt9xiQZsw7sNvDDOkUP1QrtzAx95w-ujtj7vFQEAqOMkIWKg',
+    vapidPublicKey: 'BE5_bYawwzldP4pVcvUMGAXbcTPHEcMy8n4j2XX2__TMdeT49B4AzO95Uf8nWzpkRmRJnGVReX5iLPpln05AV1M',
   });
 })();
